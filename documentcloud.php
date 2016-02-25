acb4bf9dc3f8f4aeb22a5fe30028aa3059762b5f
Justin Reese <justin@justinreese.com>
2016-02-25 17:42:53 -0600

    Bump version to 0.4.2

--- documentcloud.php
+++ documentcloud.php
@@ -3,7 +3,7 @@
  * Plugin Name: DocumentCloud
  * Plugin URI: https://www.documentcloud.org/
  * Description: Embed DocumentCloud resources in WordPress content.
- * Version: 0.4.1
+ * Version: 0.4.2
  * Authors: Chris Amico, Justin Reese
  * License: GPLv2
 ***/
