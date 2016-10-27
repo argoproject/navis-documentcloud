ae2d74cf6b501e31d9836874e73f5d05b2f333cb
Justin Reese <justin@justinreese.com>
2016-10-27 09:54:40 -0500

    Queue up version 0.4.4

--- documentcloud.php
+++ documentcloud.php
@@ -3,7 +3,7 @@
  * Plugin Name: DocumentCloud
  * Plugin URI: https://www.documentcloud.org/
  * Description: Embed DocumentCloud resources in WordPress content.
- * Version: 0.4.3
+ * Version: 0.4.4-dev
  * Authors: Chris Amico, Justin Reese
  * License: GPLv2
 ***/
